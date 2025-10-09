e81a70e1fad32c8e6dc7d9382fd76a186b9a5463
Saroj Adhikari <sarojadh1111@gmail.com>
2025-10-09 19:08:01 +0400

    initial commit

---
+++ sizeOfOperator.c
@@ -0,0 +1,12 @@
+// sizeof Operator
+#include<stdio.h>
+void main ()
+{
+	
+ int a ;
+ printf("size of integer = %d bytes\n",sizeof(int));
+  printf("size of float = %d bytes\n",sizeof(float));
+    printf("size of character = %d bytes\n",sizeof(char));
+     printf("size of double = %d bytes\n",sizeof(double));
+
+}
