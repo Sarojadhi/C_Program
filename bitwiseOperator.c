59aa43bc814263c6d36b189a183c09a16fa93b36
Saroj Adhikari <sarojadh1111@gmail.com>
2025-10-10 12:00:56 +0545

    bitwise Opreator

---
+++ bitwiseOperator.c
@@ -0,0 +1,12 @@
+
+// Example of bitwise Operator 
+ 	#include <stdio.h>
+	void main (){
+	int a = 5 , b= 3 ;
+	printf("a&b= %d\n",a&b);
+	printf("a|b = %d\n ",a|b);
+		printf("a^b = %d\n ",a^b);
+			printf("a<<1 = %d\n ",a<<1);
+}
+
+
