6628eaa0a212a3aef2143b55b5c2db451bcfec93
Saroj Adhikari <sarojadh1111@gmail.com>
2025-10-12 19:04:12 +0545

    join string

---
+++ string/string_join.c
@@ -0,0 +1,10 @@
+//String library function example
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str1[]="Birendra",str2[]="Campus";
+	strcat(str1,str2);
+    printf("str1 = %s\n",str1);
+    printf("str2 = %s",str2);
+}
